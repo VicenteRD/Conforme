0ea61102fa7c3698d65e81eccf5f78ac1b65939f
Vicente Rivera <vicente.drd@gmail.com>
2017-02-18 17:26:57 -0300

    Switching to scss to have everything compressed by Rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( source/main.scss )
+Rails.application.config.assets.precompile += %w( main.scss )
 Rails.application.config.assets.precompile += %w( show.js )
 Rails.application.config.assets.precompile += %w( form.js )
 Rails.application.config.assets.precompile += %w( associables.scss )
