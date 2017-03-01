1c085e38da20489145b6eff88dfa74dbb4ca8a87
Vicente Rivera <vicente.drd@gmail.com>
2017-03-01 00:30:05 -0300

    Fix possible issue with indicators

--- app/controllers/indicator_measurements_controller.rb
+++ app/controllers/indicator_measurements_controller.rb
@@ -1,5 +1,7 @@
 class IndicatorMeasurementsController < ApplicationController
 
+  include DatesHelper
+
   before_action :check_permissions, only: [:new, :create, :edit, :update]
 
   def show_details
