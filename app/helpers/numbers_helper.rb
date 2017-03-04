9e8b97a58981d458ebe3677f7b3d260fc91d77b7
Vicente Rivera <vicente.drd@gmail.com>
2017-03-04 19:11:59 -0300

    Move chart_data helper method to NumbersHelper

--- app/helpers/numbers_helper.rb
+++ app/helpers/numbers_helper.rb
@@ -10,4 +10,39 @@ module NumbersHelper
   def boolean_hash
     {true => 'Si', false => 'No'}
   end
+
+  def chart_data(measurements, x_buffer, field_key = 'magnitude')
+
+    # Instantiate a set of raw holders
+    raw_dates = Set.new
+    values = []
+    thresholds = []
+
+    measurements.each do |m|
+      date = m.measured_at.to_date.to_time.to_i
+
+      raw_dates.add(date)
+
+      values.push({x: date, y: m[field_key.to_sym]})
+      thresholds.push({x: date, y: m.threshold})
+    end
+
+    # Process the raw data into a hash for ease of use
+    data = {}
+
+    # We use an array that Javascript can read, instead of a set
+    data[:numeric_dates] = raw_dates.to_a
+
+    # For the hashes, we turn them into a string beforehand, and
+    # modify it to match the JS format of a dict / hash.
+    data[:values] = ruby_to_js_hash(values)
+    data[:thresholds] = ruby_to_js_hash(thresholds)
+
+    # Also add minX and maxX to ease the readability on the view itself.
+    # Y ranges are not included, because they vary from risk type to risk type.
+    data[:min_x] = data[:numeric_dates].first - x_buffer.hours.to_i
+    data[:max_x] = data[:numeric_dates].last + x_buffer.hours.to_i
+
+    data
+  end
 end
