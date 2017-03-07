94f5d3df035853ddf39635cf0a1c81ec43e1c642
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2017-03-07 15:26:27 -0300

    profile view, still needs to change the english /placeholders/ for spanish ones

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,7 @@
 class UsersController < ApplicationController
   def index
     @person = Person::User.all
-
-    # @settings = Settings::RiskSettings.first
-    #
-    # es_type = params[:type].to_sym
-    #
-    # if Risk.get_risk_types.key?(es_type)
-    #   klass = Risk.get_risk_types[es_type][:klass]
-    #
-    #   @risks = klass.order_by(significant: :desc)
-
-      render "users/index", layout: 'table'
-    # else
-    #   redirect_to '/'
-    # end
+    render "users/index", layout: 'table'
   end
 
   def show
